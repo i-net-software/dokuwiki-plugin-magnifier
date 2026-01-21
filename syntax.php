5b7b11c7ce71fb30c1fa5284b2ae37c90acac2c1
Gerry Weißbach <gerry.w@gammaproduction.de>
2026-01-21 12:53:38 +0100

    Fix require_once deprecation in syntax.php

--- syntax.php
+++ syntax.php
@@ -11,8 +11,6 @@
 if(!defined('DOKU_INC')) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
-require_once(DOKU_PLUGIN.'syntax.php');
-
 class syntax_plugin_magnifier extends DokuWiki_Syntax_Plugin {
 
     private $headers = array();
